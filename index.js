7c72a11a3b9120023978a205066e139217552b1b
amirarabirazdadeh <amirarabi1362@gmail.com>
2023-06-22 10:46:34 +0330

    add some changes5555552255552555

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// Require the framework and instantiate it
+var express = require('express')
+var app = express()
 
-// CommonJs
-const fastify = require('fastify')({
-  logger: true
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  const tenant = request.header('x-tenant');
+  response.append("XXXXXXXXXX", request.headers.x-tenant).send(JSON.stringify(request.headers))
 })
 
-// Declare a route
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
 })
 
-// Run the server!
-fastify.listen({ port: 3000 }, function (err, address) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-  // Server is now listening on ${address}
-})
+
+exports.handler =  async function(event, context) {
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+  return "Hello World!!"
+}
