964936d9f0f059474fe6f8ad527145ecda964e51
amirarabirazdadeh <amirarabi1362@gmail.com>
2023-06-22 09:29:28 +0330

    add some changes5555

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-var express = require('express')
-var app = express()
+// CommonJs
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  const tenant = request.header('x-tenant');
-  response.append("XXXXXXXXXX", tenant).send(JSON.stringify(request.headers))
-})
+exports.handler =  async function(event, context) {
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+  return "Hello World!!"
+}
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
