8dd3ef882bb767eff7ef3a60bbc50558e7f1adb4
amirarabirazdadeh <amirarabi1362@gmail.com>
2023-06-22 11:42:02 +0330

    add some changes5555552255552555201552

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-const handler = (event, context, callback) => {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
-  return "Hello World!!"
-}
+// var express = require('express')
+// var app = express()
 
-handler();
+// app.set('port', (process.env.PORT || 5000))
+// app.use(express.static(__dirname + '/public'))
+
+// app.get('/', function(request, response) {
+//   response.send('Hello World!!!')
+// })
+
+// app.listen(app.get('port'), function() {
+//   console.log("Node app is running at localhost:" + app.get('port'))
+// })
+
+// Require the framework and instantiate it
+
+
+// CommonJs
+const fastify = require('fastify')({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world....' })
+})
+
+// Run the server!
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
+
+
