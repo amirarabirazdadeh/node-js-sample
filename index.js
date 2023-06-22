88a29f18533278023b868a17ef2a12de9252b406
amirarabirazdadeh <amirarabi1362@gmail.com>
2023-06-22 09:49:17 +0330

    add some changes5555552

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// CommonJs
-
-
-exports.handler =  async function(event, context) {
+exports.handler = function (event, context, callback) {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2))
   return "Hello World!!"
-}
-
+};
