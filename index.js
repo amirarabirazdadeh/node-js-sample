eb0a1cb4acaa550639245c6c38f17909b149f03c
amirarabirazdadeh <amirarabi1362@gmail.com>
2023-06-22 10:24:06 +0330

    add some changes555555225555

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const handler = function (event, context, callback) {
+
+module.exports.handler =  async (event, context, callback) => {
   console.log("EVENT: \n" + JSON.stringify(event, null, 2))
   return "Hello World!!"
-};
-
-handler();
+}
