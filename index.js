cdfe5f42768a4de00036a57d0e039ffb14beda2d
amirarabirazdadeh <amirarabi1362@gmail.com>
2023-06-24 13:15:11 +0330

    add some changes555555225555255520155252525500

--- index.js
+++ index.js
@@ -1,92 +1,18 @@
-// var express = require('express')
-// var app = express()
-
-// app.set('port', (process.env.PORT || 5000))
-// app.use(express.static(__dirname + '/public'))
-
-// app.get('/', function(request, response) {
-//   response.send('Hello World!!!')
-// })
-
-// app.listen(app.get('port'), function() {
-//   console.log("Node app is running at localhost:" + app.get('port'))
-// })
-
-// Require the framework and instantiate it
-
-
-
-
-//
-  // CommonJs
-const app = require('fastify')({
+// CommonJs
+const fastify = require('fastify')({
   logger: true
 })
 
 // Declare a route
-app.post('/', function (request, reply) {
- // console.log(request);
-  reply
-  .code(200)
-  .headers({
-      "XXXXXXXXXXXB": `OOOOOOOOOOOOOOOB`,
-      "X-Amz-Client-Context": `eyJYWFhYWFgiOiJRUVFRUVFRUVEifQ==`,
-      "X-Amz-Xccccc": `=========`
-  })
-  .send({
-    message: "POST Hello World!",
-    context: { headers: { "x-tenant": "root" } }
-  })
-});
-
-app.post('/goodbye', function (request, reply) {
-  // console.log(request);
-   reply
-   .code(200)
-   .headers({
-       "XXXXXXXXXXXB": `OOOOOOOOOOOOOOOB`,
-       "X-Amz-Client-Context": `eyJYWFhYWFgiOiJRUVFRUVFRUVEifQ==`,
-       "X-Amz-Xccccc": `=========`
-   })
-   .send({
-     message: "Goodbye World!",
-     context: { headers: { "x-tenant": "root" } }
-   })
- });
-
- app.get('/', function (request, reply) {
-  // console.log(request);
-   reply
-   .code(200)
-   .headers({
-       "XXXXXXXXXXXB": `OOOOOOOOOOOOOOOB`,
-       "X-Amz-Client-Context": `eyJYWFhYWFgiOiJRUVFRUVFRUVEifQ==`,
-       "X-Amz-Xccccc": `=========`
-   })
-   .send({
-     message: "GET Hello World!",
-     context: { headers: { "x-tenant": "root" } }
-   })
- });
- 
-//  const test = async () => {
-// const response = await app.inject({
-//   method: 'POST',
-//   url: '/goodbye'
-// });
-
-//   console.log('body: ', JSON.parse(response.body))
-// }
-// test();
-
-
-//Run the server!
-app.listen({ port: 3000 }, function (err, address) {
-  if (err) {
-    app.log.error(err)
-    process.exit(1)
-  }
-  //Server is now listening on ${address}
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'world' })
 })
 
-
+// Run the server!
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
