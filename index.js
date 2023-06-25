a043cc1f75bc8845504e2f0c0bc04d47cbfde50f
amirarabirazdadeh <amirarabi1362@gmail.com>
2023-06-25 09:03:59 +0330

    add some changes555555225555255520155252525500555

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-// CommonJs
-const fastify = require('fastify')({
-  logger: true
-})
-
-// Declare a route
-fastify.get('/', function (request, reply) {
-  reply.send({ hello: 'world' })
-})
-
-/**
- * Run the server!
- */
-const start = async () => {
-  try {
-    const response = await fastify.inject({
-      method: 'GET',
-      url: '/'
+// using the http module
+let http = require('http'),
+ 
+// look for PORT environment variable, 
+// else look for CLI argument,
+// else use hard coded value for port 8080
+port = process.env.PORT || process.argv[2] || 3000;
+ 
+// create a simple server
+let server = http.createServer(function (req, res) {
+ 
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('hello heroku!', 'utf-8');
+        res.end();
+ 
     });
-   //console.log(response.body);
-   return JSON.parse(response.body);
-  } catch (err) {
-    fastify.log.error(err)
-    process.exit(1)
-  }
-}
-start()
-
+ 
+// listen on the port
+server.listen(port, function () { 
+    console.log('app up on port: ' + port);
+ 
+});
